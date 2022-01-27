71f8f3b4bafc27d9e78fe6362e9a0795f3143586
Ignacio Grassini <ignacio.grassini@attackiq.com>
2022-01-27 13:49:25 -0300

    feat: :building_construction: Dockerize BE app

--- backend/config.js
+++ backend/config.js
@@ -4,6 +4,17 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 // access config var
-const { PORT, DB, TOKEN_SECRET, SALT } = process.env
+const {
+  PORT,
+  DB_HOST,
+  DB_USER,
+  DB_PASSWORD,
+  DB_NAME,
+  DB_PORT,
+  TOKEN_SECRET,
+  SALT,
+} = process.env
+
+const DB = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`
 
 module.exports = { PORT, DB, TOKEN_SECRET, SALT }
